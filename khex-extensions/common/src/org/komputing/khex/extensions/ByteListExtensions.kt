375886d31c9fbe3416fa99e2671a93a167f4491d
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-04-10 12:23:13 +0200

    Mulitplatform implementation - Reviseted the whole project organization to make it multiplatform compatible - Moved the extensions functions inside the khex-extensions module - Created an object implementation inside the khex module

---
+++ khex-extensions/common/src/org/komputing/khex/extensions/ByteListExtensions.kt
@@ -0,0 +1,21 @@
+package org.komputing.khex.extensions
+
+import org.komputing.khex.Hex
+
+/**
+ * Converts [this] [List] of bytes into its hexadecimal string representation prepending to it the given [prefix].
+ *
+ * Note that by default the 0x prefix is prepended to the result of the conversion.
+ * If you want to have the representation without the 0x prefix, use the [toNoPrefixHexString] method or
+ * pass to this method an empty [prefix].
+ */
+fun List<Byte>.toHexString(prefix: String = "0x"): String {
+    return Hex.encode(this.toByteArray(), prefix)
+}
+
+/**
+ * Converts [this] [List] of bytes into its hexadecimal representation without prepending any prefix to it.
+ */
+fun List<Byte>.toNoPrefixHexString(): String {
+    return toHexString("")
+}
