fe8e8edb551d5b7fb303c835413ae3793a2cf2c1
Luca Spinazzola <mspinluca@gmail.com>
2023-05-17 00:31:00 -0400

    Add native targets (#14)
    * add gradle template to gitignore
    
    * kotlin, assertk, and gradle updates
    
    * add native targets
    
    * use new memory model

--- buildSrc/src/main/kotlin/mpp-module.gradle.kts
+++ buildSrc/src/main/kotlin/mpp-module.gradle.kts
@@ -11,6 +11,14 @@ plugins {
     id("maven-publish")
 }
 
+val nativeTargets = arrayOf(
+    "linuxX64",
+    "macosX64", "macosArm64",
+    "iosArm32", "iosArm64", "iosX64", "iosSimulatorArm64",
+    "tvosArm64", "tvosX64", "tvosSimulatorArm64",
+    "watchosArm32", "watchosArm64", "watchosX86", "watchosX64", "watchosSimulatorArm64",
+)
+
 kotlin {
     explicitApi()
     targets {
@@ -39,6 +47,9 @@ kotlin {
                     }
                 }
             }
+        }
+        for (target in nativeTargets) {
+            targets.add(presets.getByName(target).createTarget(target))
         }
     }
 
